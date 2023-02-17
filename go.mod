157fe40d2e5b0e827199e0a0b29a0e04d56af29d
Alisa <kotova.alisa@gmail.com>
2023-02-17 23:42:22 +0300

    version 2.0.0 created

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/alisakotova/version
+module github.com/alisakotova/version/v2
 
 go 1.19
--- version.go
+++ version.go
@@ -3,6 +3,6 @@ package version
 import "fmt"
 
 // Version - print current module version
-func Version() {
-	fmt.Println("v1.0.0")
+func Version() string {
+	return "v2.0.0"
 }
